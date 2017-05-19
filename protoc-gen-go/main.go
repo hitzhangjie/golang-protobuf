0c5ca2dc962fa22f9b683810f5928d68f7b312bf
hitzhangjie <hit.zhangjie@gmail.com>
2017-05-19 13:55:38 +0800

    commit my comments

--- protoc-gen-go/main.go
+++ protoc-gen-go/main.go
@@ -1,51 +1,3 @@
-// Go support for Protocol Buffers - Google's data interchange format
-//
-// Copyright 2010 The Go Authors.  All rights reserved.
-// https://github.com/golang/protobuf
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are
-// met:
-//
-//     * Redistributions of source code must retain the above copyright
-// notice, this list of conditions and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above
-// copyright notice, this list of conditions and the following disclaimer
-// in the documentation and/or other materials provided with the
-// distribution.
-//     * Neither the name of Google Inc. nor the names of its
-// contributors may be used to endorse or promote products derived from
-// this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-// protoc-gen-go is a plugin for the Google protocol buffer compiler to generate
-// Go code.  Run it by building this program and putting it in your path with
-// the name
-// 	protoc-gen-go
-// That word 'go' at the end becomes part of the option string set for the
-// protocol compiler, so once the protocol compiler (protoc) is installed
-// you can run
-// 	protoc --go_out=output_directory input_directory/file.proto
-// to generate Go bindings for the protocol defined by file.proto.
-// With that input, the output will be written to
-// 	output_directory/file.pb.go
-//
-// The generated code is documented in the package comment for
-// the library.
-//
-// See the README and documentation for protocol buffers to learn more:
-// 	https://developers.google.com/protocol-buffers/
 package main
 
 import (
@@ -57,40 +9,49 @@ import (
 )
 
 func main() {
-	// Begin by allocating a generator. The request and response structures are stored there
-	// so we can do error handling easily - the response structure contains the field to
-	// report failure.
+	// 首先创建一个代码生成器generator，CodeGeneratorRequest、CodeGeneratorResponse
+	// 结构体都被保存在generator中，CodeGenerateResponse中保存着代码生成过程中
+	// 的错误状态信息，因此我们可以通过这个结构体提取错误状态并进行错误处理
 	g := generator.New()
 
+	// 从标准输入中读取CodeGeneratorRequest信息（标准输入已经被重定向到了父进程
+	// protoc进程创建的管道stdout_pipe的读端，父进程会从管道的写端写入该请求信息）
 	data, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
 		g.Error(err, "reading input")
 	}
 
+	// 读取到的数据时串行化之后的CodeGeneratorRequest，将其反串行化成CodeGeneratorRequest
 	if err := proto.Unmarshal(data, g.Request); err != nil {
 		g.Error(err, "parsing input proto")
 	}
 
+	// 检查CodeGeneratorRequest中待生成的源代码文件数量，数量为0则无需生成
 	if len(g.Request.FileToGenerate) == 0 {
 		g.Fail("no files to generate")
 	}
 
+	// 将CodeGeneratorRequest中传递给代码生成器的参数设置到protoc插件的代码生成器中
 	g.CommandLineParameters(g.Request.GetParameter())
 
-	// Create a wrapped version of the Descriptors and EnumDescriptors that
-	// point to the file that defines them.
+	// 前面的proto.Unmarshal(...)操作将stdin中的请求反串行化成了CodeGeneratorRequest，
+	// 这里的g.WrapTypes()将请求中的一些descriptors进行进一步封装，方便后面引用
 	g.WrapTypes()
 
 	g.SetPackageNames()
 	g.BuildTypeNameMap()
 
+	// 生成所有的源代码文件
 	g.GenerateAllFiles()
 
-	// Send back the results.
+	// 将CodeGeneratorResponse对象进行串行化处理
 	data, err = proto.Marshal(g.Response)
 	if err != nil {
 		g.Error(err, "failed to marshal output proto")
 	}
+	// 将串行化之后的CodeGenerateResponse对象数据写入标准输出（标准输出已经被
+	// 重定向到了父进程protoc进程创建的管道stdin_pipe的写端，父进程从管道的读
+	// 端读取这里的响应）
 	_, err = os.Stdout.Write(data)
 	if err != nil {
 		g.Error(err, "failed to write output proto")
